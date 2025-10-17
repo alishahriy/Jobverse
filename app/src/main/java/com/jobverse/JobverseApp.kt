93f0f35417f4a99c4fb244df8f75baa56759e534
alishahriy <65369348+alishahriy@users.noreply.github.com>
2025-10-17 14:50:30 +0330

    Create JobverseApp.kt

---
+++ app/src/main/java/com/jobverse/JobverseApp.kt
@@ -0,0 +1,10 @@
+package com.jobverse
+
+import android.app.Application
+
+class JobverseApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // Initialize repositories, analytics, etc. (if any)
+    }
+}
