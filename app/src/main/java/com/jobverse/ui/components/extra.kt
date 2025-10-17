2769c2ea565542ba0b7e7fb99f5d01f1bdd8be03
alishahriy <65369348+alishahriy@users.noreply.github.com>
2025-10-17 16:56:12 +0330

    Create extra.kt

---
+++ app/src/main/java/com/jobverse/ui/components/extra.kt
@@ -0,0 +1,14 @@
+package com.jobverse.ui.components
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Loading() {
+    Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        CircularProgressIndicator()
+    }
+}
